01a63e69c477d976c0aa525385cf95a99c4847bf
Daniel <djperrefort@gmail.com>
2023-03-23 16:37:13 -0400

    Adds placeholder tests

---
+++ tests/test_placeholder.py
@@ -0,0 +1,12 @@
+"""This is a placeholder test so the CI workflow has something to run while the
+real tests are being written.
+"""
+
+from unittest import TestCase
+
+
+class Placeholder(TestCase):
+    """A placeholder test class"""
+
+    def runTest(self) -> None:
+        """THis test will always pass"""
