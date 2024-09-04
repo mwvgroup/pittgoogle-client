21204ef3c5d53ecf339e428644fbf4ce554cf15d
Troy Raen <raen@ipac.caltech.edu>
2024-09-04 02:16:40 -0700

    add Alert.from_cloud_functions

--- pittgoogle/alert.py
+++ pittgoogle/alert.py
@@ -58,10 +58,49 @@ class Alert:
     )
     path: Path | None = attrs.field(default=None)
     # Use "Union" because " | " is throwing an error when combined with forward references.
+    context: Union[
+        "google.cloud.functions_v1.context.Context", types_._FunctionsContextLike, None
+    ] = attrs.field(default=None)
     _dataframe: Union["pd.DataFrame", None] = attrs.field(default=None)
     _schema: Schema | None = attrs.field(default=None, init=False)
 
     # ---- class methods ---- #
+    @classmethod
+    def from_cloud_functions(
+        cls,
+        event: Mapping,
+        context: "google.cloud.functions_v1.context.Context",
+        schema_name: str | None = None,
+    ):
+        """Create an `Alert` from an 'event' and 'context', as received by a Cloud Functions module.
+
+        Argument definitions copied from https://cloud.google.com/functions/1stgendocs/tutorials/pubsub-1st-gen
+
+        Args:
+            event (dict):
+                The dictionary with data specific to this type of event. The `@type` field maps to
+                `type.googleapis.com/google.pubsub.v1.PubsubMessage`. The `data` field maps to the
+                PubsubMessage data in a base64-encoded string. The `attributes` field maps to the
+                PubsubMessage attributes if any is present.
+            context (google.cloud.functions.Context):
+                Metadata of triggering event including `event_id` which maps to the PubsubMessage
+                messageId, `timestamp` which maps to the PubsubMessage publishTime, `event_type` which
+                maps to `google.pubsub.topic.publish`, and `resource` which is a dictionary that
+                describes the service API endpoint pubsub.googleapis.com, the triggering topic's name,
+                and the triggering event type `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
+        """
+        return cls(
+            msg=types_.PubsubMessageLike(
+                # data is required. the rest should be present in the message, but use get to be lenient
+                data=base64.b64decode(event["data"]),
+                attributes=event.get("attributes", {}),
+                message_id=context.event_id,
+                publish_time=cls._str_to_datetime(context.timestamp),
+            ),
+            context=context,
+            schema_name=schema_name,
+        )
+
     @classmethod
     def from_cloud_run(cls, envelope: Mapping, schema_name: str | None = None) -> "Alert":
         """Create an `Alert` from an HTTP request envelope containing a Pub/Sub message, as received by a Cloud Run module.
