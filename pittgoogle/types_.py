e7c10dc237b588a69cca1380fee6e04bda5456e4
Troy Raen <raen@ipac.caltech.edu>
2024-09-04 02:17:20 -0700

    add types_._FunctionsContextLike

--- pittgoogle/types_.py
+++ pittgoogle/types_.py
@@ -43,3 +43,34 @@ class PubsubMessageLike:
     """Timestamp of the published message."""
     ordering_key: str | None = attrs.field(default=None)
     """Pub/Sub ordering key of the published message."""
+
+
+@attrs.define(frozen=True)
+class _FunctionsContextLike:
+    """Container that mimics the "context" metadata of a Cloud Functions triggering event.
+
+    Parameters definitions copied from https://cloud.google.com/functions/1stgendocs/tutorials/pubsub-1st-gen
+
+    Parameters;
+        event_id (str):
+            Maps to PubsubMessage 'messageId'.
+        timestamp (str):
+            Maps to PubsubMessage 'publishTime'.
+        event_type (str):
+            Maps to `google.pubsub.topic.publish`.
+        resource (dict):
+            Describes the service API endpoint pubsub.googleapis.com, the triggering
+            topic's name, and the triggering event type
+            `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
+    """
+
+    event_id: str
+    timestamp: str
+    event_type: str = attrs.field(default="google.pubsub.topic.publish")
+    resource: dict = attrs.field(
+        default={
+            "name": "projects/mock-project/topics/mock-topic",
+            "service": "pubsub.googleapis.com",
+            "type": "type.googleapis.com/google.pubsub.v1.PubsubMessage",
+        }
+    )
