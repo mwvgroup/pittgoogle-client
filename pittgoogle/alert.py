1610edfcb1e144aa7f6ef54a8d12344c74d0b8f3
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-08-29 12:26:16 -0400

    update yaml file to make objectid a single dict

--- pittgoogle/alert.py
+++ pittgoogle/alert.py
@@ -597,26 +597,13 @@ class Alert:
             any:
                 The value in the :attr:`Alert.dict` corresponding to the field.
         """
-        survey_field = self.schema.map.get(field)  # str, list[str], list[dict], or None
+        survey_field = self.schema.map.get(field)  # str, list[str], dict, or None
 
         if survey_field is None:
             return default
 
         if isinstance(survey_field, str):
             return self.dict.get(survey_field, default)
-
-        if isinstance(survey_field, list) and all(isinstance(x, dict) for x in survey_field):
-            # This was implemented specifically for LSST objectid.
-            # We assume that the list values are dicts with exactly two elements
-            # and that only one of these will point to a non-null value in the alert.
-            for subdict in survey_field:
-                for survey_fields in subdict.values():
-                    alert_value = (self.dict.get(survey_fields[0]) or {}).get(
-                        survey_fields[1], None
-                    )
-                    if alert_value:
-                        return alert_value
-            return default
 
         # if survey_field is not one of the expected types, the schema map is malformed
         # maybe this was intentional, but we don't know how to handle it here
@@ -663,25 +650,25 @@ class Alert:
                 list[str] if this is a nested field and `name_only` is False, else str with the
                 final field name only.
         """
-        survey_field = self.schema.map.get(field)  # str, list[str], list[dict], or None
+        survey_field = self.schema.map.get(field)  # str, list[str], dict[str, list[str]], or None
 
         if survey_field is None:
             return default
 
-        if isinstance(survey_field, list) and all(isinstance(x, dict) for x in survey_field):
-            # This was implemented specifically for LSST objectid.
-            # We assume that the list values are dicts with exactly two elements
-            # and that only one of these will point to a non-null value in the alert.
-            for subdict in survey_field:
-                for survey_fields in subdict.values():
-                    alert_value = (self.dict.get(survey_fields[0]) or {}).get(
-                        survey_fields[1], None
-                    )
-                    if alert_value is not None:
-                        return survey_fields[-1] if name_only else survey_fields
-            return default
-        elif name_only and isinstance(survey_field, list):
+        if name_only and isinstance(survey_field, list):
             return survey_field[-1]
+
+        if isinstance(survey_field, dict):
+            # This was implemented specifically for LSST objectid.
+            # We assume that the dict values are lists with exactly two elements
+            # and that only one of these will point to a non-null value in the alert.
+            for survey_fields in survey_field.values():
+                # Check whether this item points to a non-null value in the alert. If so, return the key.
+                alert_value = self.dict.get(survey_fields[0], {}).get(survey_fields[1], None)
+                if alert_value:
+                    if name_only:
+                        return survey_fields[-1]
+                    return survey_fields
 
         return survey_field
 
