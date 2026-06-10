e1a3ae73931d4f2baef0e9cc8fb619bd8d38660f
Chris Hernandez <hernandezchristopher.13@gmail.com>
2026-06-09 19:50:30 -0700

    add pytest fixtures for unit testing RAPID alerts

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 """Fixtures for unit tests."""
+
 import io
 import json
 from pathlib import Path
@@ -11,7 +12,6 @@ import pittgoogle
 
 import load_data
 
-
 TESTS_DATA_DIR = Path(__file__).parent / "data"
 SCHEMAS_DIR = pittgoogle.__package_path__ / "schemas"
 
@@ -22,7 +22,7 @@ SCHEMAS_DIR = pittgoogle.__package_path__ / "schemas"
 @pytest.fixture
 def survey_names() -> list[str]:
     """List of all survey names supported by pittgoogle."""
-    return ["lsst", "ztf", "lvk"]
+    return ["lsst", "ztf", "lvk", "rapid"]
 
 
 # [FIXME] Move most of this to load_data?
@@ -121,10 +121,46 @@ def sample_alerts_ztf() -> list[load_data.TestAlert]:
 
 
 @pytest.fixture
+def sample_alerts_rapid() -> list[load_data.TestAlert]:
+    """List of all RAPID sample alerts."""
+    survey = "rapid"
+    alert_paths = _get_sample_alert_paths(survey)
+    alerts = []
+    for alert_path in alert_paths:
+        # Expecting alert_path names like "rapid.v01_00.avro".
+        schema_fname = alert_path.with_suffix(".alert.avsc").name
+        schema_version = alert_path.suffixes[0].strip(".")
+        major, minor = schema_version.strip("v").split("_")
+        schema_path = SCHEMAS_DIR / survey / major / minor / schema_fname
+        schema = fastavro.schema.load_schema(schema_path)
+        alert_bytes = alert_path.read_bytes()
+        bytes_io = io.BytesIO(alert_bytes[5:])
+        alerts.append(
+            load_data.TestAlert(
+                schema_name=survey,
+                schema_version=schema_version,
+                survey=survey,
+                path=alert_path,
+                bytes_=alert_bytes,
+                dict_=fastavro.schemaless_reader(bytes_io, schema),
+            )
+        )
+    return alerts
+
+
+@pytest.fixture
+def sample_alert_rapid_latest(sample_alerts_rapid) -> load_data.TestAlert:
+    latest_version = sorted(sa.schema_version for sa in sample_alerts_rapid)[-1]
+    for sample_alert in sample_alerts_rapid:
+        if sample_alert.schema_version == latest_version:
+            return sample_alert
+
+
+@pytest.fixture
 def sample_alerts(
-    sample_alerts_lsst, sample_alerts_lvk, sample_alerts_ztf
+    sample_alerts_lsst, sample_alerts_lvk, sample_alerts_ztf, sample_alerts_rapid
 ) -> list[load_data.TestAlert]:
-    return [*sample_alerts_lsst, *sample_alerts_lvk, *sample_alerts_ztf]
+    return [*sample_alerts_lsst, *sample_alerts_lvk, *sample_alerts_ztf, *sample_alerts_rapid]
 
 
 @pytest.fixture
